7f4ada501a54031c4b5346a85b7d75ea743619bf
MrMst <mst@example.com>
2018-02-25 17:41:33 +0100

    feat: Add book fetching via API

---
+++ src/ListBooks.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class ListBooks extends Component {
+  render() {
+    return(
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>MyReads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ListBooks
