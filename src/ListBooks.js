c5e0990eacaefdea53df11a1dbb17d1b2fbd42fd
MrMst <mst@example.com>
2018-02-25 22:08:07 +0100

    feat: Create components

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+import Bookshelf from './Bookshelf'
 
 class ListBooks extends Component {
   render() {
+
+    let currentlyReading = this.props.books.filter((book) => { return book.shelf === 'currentlyReading'})
+    let wantToRead = this.props.books.filter((book) => { return book.shelf === 'wantToRead'})
+    let read = this.props.books.filter((book) => { return book.shelf === 'read'})
+
     return(
       <div className="list-books">
         <div className="list-books-title">
@@ -9,7 +15,18 @@ class ListBooks extends Component {
         </div>
         <div className="list-books-content">
           <div>
-
+            <Bookshelf
+              books={currentlyReading}
+              title='Currently Reading'
+            />
+            <Bookshelf
+              books={read}
+              title='Read'
+            />
+            <Bookshelf
+              books={wantToRead}
+              title='Want To Read'
+            />
           </div>
         </div>
       </div>
